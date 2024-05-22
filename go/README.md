fbc03de1598854d9e3f1aa3212d9ff27f48da162
RWCannell <regancannell@gmail.com>
2024-05-22 10:37:14 +0200

    Added source code for go tutorial

---
+++ go/README.md
@@ -0,0 +1,5 @@
+## RabbitMQ Apps
+This repository contains simple applications to test connections to a RabbitMQ instance (hosted remotely or locally). Most of the knowledge is obtained directly from the [RabbitMQ documentation](https://www.rabbitmq.com/tutorials). 
+
+### Go
+The sending and receiving of messages to a remote RabbitMQ instance is tested using the Go programming language. The tutorial for this application can be found over [here](https://www.rabbitmq.com/tutorials/tutorial-one-go).
---
+++ go/receiver/receive.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	username := os.Getenv("USERNAME")
+	password := os.Getenv("PASSWORD")
+	ipAddress := os.Getenv("IP_ADDRESS")
+	port := os.Getenv("PORT")
+	vHost := os.Getenv("V_HOST")
+
+	conn, err := amqp.Dial("amqp://" + username + ":" + password + "@" + ipAddress + ":" + port + "/" + vHost)
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	var forever chan struct{}
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+}
